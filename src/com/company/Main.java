eadc09cb82d8fdfeef24f4a6d34b953c2a6ef18c
Ferraz@myPC_SourceTree <x12586@yahoo.com>
2017-10-02 19:13:27 +0100

    Init commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+// Aula de manipulação de Files do tipo TXT
+/* Procedimentos:
+    1- criar uma string com o caminho e nome de um ficheiro a criar no disco
+    2- Usar um método nativo para criar o ficheiro
+    3- Usar um método nativo para ler o ficheiro
+* */
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
